0730d4aa8eceb61070e98e0e00f1f068dd5307be
Penis <ckatuhkoo@gmail.com>
2016-07-26 21:51:38 +0300

    Program.cs

--- Dagon_Stealer/Program.cs
+++ Dagon_Stealer/Program.cs
@@ -1,4 +1,3 @@
-
 using System;
 using System.Linq;
 using Ensage.Common.Menu;
@@ -7,14 +6,16 @@ using SharpDX;
 using SharpDX.Direct3D9;
 using System.Collections.Generic;
 
-namespace Dagon_Stealer {
-    class Program {
+namespace Dagon_Stealer
+{
+    class Program
+    {
         private static Font _text;
         private static readonly int[] Penis = new int[5] { 400, 500, 600, 700, 800 };
         private static readonly int[] ShitDickFuck = new int[5] { 600, 650, 700, 750, 800 };
         private static readonly Menu Menu = new Menu("Dagon Stealer", "dagonstealer", true);
 
-        private static List<string> Ignore = new List<string> 
+        private static List<string> Ignore = new List<string>
         {
             "modifier_item_sphere_target",
             "modifier_templar_assassin_refraction_absorb",
@@ -23,11 +24,16 @@ namespace Dagon_Stealer {
             "modifier_nyx_assassin_spiked_carapace",
             "modifier_item_blade_mail_reflect",
             "modifier_item_lotus_orb_active",
+            "modifier_item_lotus_orb_active",
+            "modifier_nyx_assassin_spiked_carapace",
+            "modifier_medusa_stone_gaze_stone",
+            "modifier_winter_wyvern_winters_curse",
         };
 
         private static Dictionary<string, bool> enemies = new Dictionary<string, bool>();
 
-        private static void Main(string[] args) {
+        private static void Main(string[] args)
+        {
             Menu.AddItem(new MenuItem("keyBind", "Main Key").SetValue(new KeyBind('K', KeyBindType.Toggle, true)));
             Menu.AddItem(new MenuItem("enemies", "Heroes").SetValue(new HeroToggler(enemies)));
             Menu.AddToMainMenu();
@@ -35,16 +41,19 @@ namespace Dagon_Stealer {
             Game.OnUpdate += Vagina;
         }
 
-        private static void Vagina(EventArgs Tits) {
+        private static void Vagina(EventArgs Tits)
+        {
             if (!Game.IsInGame)
                 return;
 
-            var me = ObjectMgr.LocalHero;
+            var me = ObjectManager.LocalHero;
             if (me == null || !me.IsAlive)
                 return;
 
-            foreach (var v in ObjectMgr.GetEntities<Hero>().Where(x => x.Team != me.Team && !x.IsIllusion)) {
-                if (!enemies.ContainsKey(v.Name)) {
+            foreach (var v in ObjectManager.GetEntities<Hero>().Where(x => x.Team != me.Team && !x.IsIllusion))
+            {
+                if (!enemies.ContainsKey(v.Name))
+                {
                     if (enemies.Count > 4) enemies.Clear();
                     enemies.Add(v.Name, true);
                     Menu.Item("enemies").SetValue(new HeroToggler(enemies));
@@ -52,17 +61,20 @@ namespace Dagon_Stealer {
             }
 
             var dagon = me.Inventory.Items.FirstOrDefault(Anal => Anal.Name.Contains("item_dagon"));
-            var enemy = ObjectMgr.GetEntities<Hero>()
+            var enemy = ObjectManager.GetEntities<Hero>()
                         .Where(Fuck => Fuck.Team != me.Team && Fuck.IsAlive && Fuck.IsVisible && !Fuck.IsIllusion && !Fuck.UnitState.HasFlag(UnitState.MagicImmune))
                         .ToList();
 
             if (me.Inventory.Items.Any(v => v.IsChanneling) || me.Spellbook.Spells.Any(v => v.IsChanneling)) return;
 
-            foreach (var v in enemy) {
+            foreach (var v in enemy)
+            {
                 var linkens = v.Inventory.Items.FirstOrDefault(Gay => Gay.Name == "item_sphere");
 
-                if (dagon != null && Menu.Item("keyBind").GetValue<KeyBind>().Active == true && Menu.Item("enemies").GetValue<HeroToggler>().IsEnabled(v.Name)) {
-                    if (dagon.Cooldown == 0 && me.Mana > dagon.ManaCost) {
+                if (dagon != null && Menu.Item("keyBind").GetValue<KeyBind>().Active == true && Menu.Item("enemies").GetValue<HeroToggler>().IsEnabled(v.Name))
+                {
+                    if (dagon.Cooldown == 0 && me.Mana > dagon.ManaCost)
+                    {
                         if ((linkens != null && linkens.Cooldown == 0) || v.Modifiers.Any(x => Ignore.Contains(x.Name)))
                             return;
                         var range = ShitDickFuck[dagon.Level - 1];
@@ -74,7 +86,8 @@ namespace Dagon_Stealer {
             }
         }
 
-        private static float LowUsageDistance(dynamic A, dynamic B) {
+        private static float LowUsageDistance(dynamic A, dynamic B)
+        {
             if (!(A is Unit || A is Vector3)) throw new ArgumentException("Not valid parameters, Accepts Unit/Vector3 only", "A");
             if (!(B is Unit || B is Vector3)) throw new ArgumentException("Not valid parameters, Accepts Unit/Vector3 only", "B");
             if (A is Unit) A = A.Position;
